52894e041503dc81e0865e8304ba88e0cde88205
Max Vilà Ruiz <max16506@gmail.com>
2023-07-16 20:36:29 +0200

    String-02

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,10 @@
+//Maneras de crear una variable "string"
+const producto = 'Monitor 20 pulgadas';
+const producto2 = "Monitor 20\""; //para poder poner las """ dentro del String
+const producto3 = String('Monitor 24 pulgadas');
+const producto4 = new String('Monitor de 27 pulgadas');
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
+console.log(producto4);
---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,7 @@
+//String Methods --> nombre de var str + . + str method
+const producto = 'Monitor 20'
+
+console.log(producto);
+console.log(producto.lenght); //Para conocer la cantidad de letras de una String
+console.log(producto.index0('Monitor')); //Para saber que letra está en la posición 0
+console.log(producto.includes('Tablet')); //Para preguntar booleanamente si la var str incluye la palabra ('x')
