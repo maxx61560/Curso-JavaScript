4105ddc7dec2cc4ac208c6ed5b9024bf9d3578c6
Max Vilà Ruiz <max16506@gmail.com>
2023-07-16 19:25:05 +0200

    first commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,10 @@
+// const nombre = prompt('Cual es tu nombre'); //Input para preguntar al usuario (const = crear variable que no varia durante la ejecución del sistema)
+// document.querySelector('.contenido').innerHTML = `${nombre} está aprendiend JavaScript moderno`; //Toma el nombre y lo muestra por pantalla
+// const producto = 'Monitor 24 pulgadas'; //Asigna el valor hacia la variable "producto"
+
+console.log('Hola mundo');
+function hola() {
+    console.log('Hola mundo')
+    console.log('Hola')
+    console.log('mundo')
+}
