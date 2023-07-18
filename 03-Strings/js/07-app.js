6027ed136c882ec371dd135f9cc6847423877c21
Max Vilà Ruiz <max16506@gmail.com>
2023-07-18 20:13:49 +0200

    String-07

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,10 @@
+const producto = 'Monitor 20 pulgadas'
+console.log(producto.toUpperCase()); //Para transformar todo el String en mayúsculas
+console.log(producto.toLowerCase()); //Para transformar todo el String en minúsculas
+//Example:
+const email = 'COREO@GMAIL.COM';
+console.log(email.toLowerCase());
+
+const precio = 300;
+console.log(precio);
+console.log(precio.toString()); //Para pasar un Int a String
